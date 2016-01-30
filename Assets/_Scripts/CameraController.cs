208fa322f80f5cb5ec0037e22dc7fc1b1cc718cb
Christian Dinh <Christian Dinh>
2016-01-29 22:21:29 -0600

    Add delay for camera movement, add ability to increase its elevation in the editor

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -1,25 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CameraController : MonoBehaviour {
 
 	[SerializeField] private float distance;
+	[SerializeField] private float elevation;
 	[SerializeField] private float maxPitch;
 	[SerializeField] private float minPitch;
 	[SerializeField] private float pitchRate;
+	[SerializeField] private float delay;
 
+	private Queue<Quaternion> previousPlayerRotations;
 	private Transform player;
 	private float pitch;
 
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		previousPlayerRotations = new Queue<Quaternion> ();
 	}
-	
-	void Update () {
-		pitch += Input.GetAxis ("Pitch") * pitchRate * Time.deltaTime;
+
+
+
+	void FixedUpdate () {
+		previousPlayerRotations.Enqueue (player.rotation);
+
+		pitch += Input.GetAxis ("Pitch") * pitchRate * Time.fixedDeltaTime;
 		pitch = Mathf.Clamp (pitch, minPitch, maxPitch);
 
-		transform.rotation = Quaternion.Euler(player.rotation.eulerAngles + new Vector3(pitch, 0, 0));
-		transform.position = player.position - transform.forward * distance;
+		if (delay > 0) {
+			delay -= Time.deltaTime;
+		} else {
+			transform.rotation = Quaternion.Euler (previousPlayerRotations.Dequeue().eulerAngles + new Vector3 (pitch, 0, 0));
+			transform.position = player.position - transform.forward * distance + new Vector3(0, elevation, 0);
+		}
 	}
 }
