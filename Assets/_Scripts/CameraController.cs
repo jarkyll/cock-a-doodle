b792e0f1d300dce604a05b259e9554ca43e79fbd
Christian Dinh <Christian Dinh>
2016-01-29 23:53:12 -0600

    Interpolate player movement, looks v smooth my dudes

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -8,31 +8,36 @@ public class CameraController : MonoBehaviour {
 	[SerializeField] private float elevation;
 	[SerializeField] private float maxPitch;
 	[SerializeField] private float minPitch;
+	[SerializeField] private float defaultPitch;
 	[SerializeField] private float pitchRate;
 	[SerializeField] private float delay;
 
 	private Queue<Quaternion> previousPlayerRotations;
+	private Queue<Vector3> previousPlayerPositions;
 	private Transform player;
 	private float pitch;
 
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player").transform;
 		previousPlayerRotations = new Queue<Quaternion> ();
+		previousPlayerPositions = new Queue<Vector3> ();
+		pitch = defaultPitch;
 	}
-
-
 
 	void FixedUpdate () {
 		previousPlayerRotations.Enqueue (player.rotation);
+		previousPlayerPositions.Enqueue (player.position);
 
+		// Change camera pitch based on right stick input
 		pitch += Input.GetAxis ("Pitch") * pitchRate * Time.fixedDeltaTime;
 		pitch = Mathf.Clamp (pitch, minPitch, maxPitch);
 
+		// Delay the camera rotation
 		if (delay > 0) {
-			delay -= Time.deltaTime;
+			delay -= Time.fixedDeltaTime;
 		} else {
 			transform.rotation = Quaternion.Euler (previousPlayerRotations.Dequeue().eulerAngles + new Vector3 (pitch, 0, 0));
-			transform.position = player.position - transform.forward * distance + new Vector3(0, elevation, 0);
+			transform.position = previousPlayerPositions.Dequeue() - transform.forward * distance + new Vector3(0, elevation, 0);
 		}
 	}
 }
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -32,17 +32,24 @@ public class PlayerController : MonoBehaviour {
 		// Move
 		Vector3 strafeVec = transform.right * strafe * groundSpeed;
 		Vector3 walkVec   = transform.forward * walk * groundSpeed;
-		Vector3 groundVelocity = Vector3.ClampMagnitude(strafeVec + walkVec, groundSpeed);
-		rb.velocity = groundVelocity + new Vector3(0, rb.velocity.y, 0);
+
+		Vector3 groundVel = Vector3.ClampMagnitude (strafeVec + walkVec, groundSpeed);
+		Vector3 currentGroundVel = new Vector3 (rb.velocity.x, 0, rb.velocity.z);
+		groundVel = Vector3.Lerp (currentGroundVel, groundVel, 0.1f);
+
+		// Change planar velocity while preserving jump velocity
+		rb.velocity = groundVel + new Vector3(0, rb.velocity.y, 0);
 
 		// Turn
-		//rb.AddRelativeTorque (0, yaw * angularSpeed, 0);
-		rb.angularVelocity = new Vector3 (0, yaw * angularSpeed, 0);
+		float turnVel = Mathf.Lerp(rb.angularVelocity.y, yaw * angularSpeed, 0.2f);
+		rb.angularVelocity = new Vector3 (0, turnVel, 0);
 
 		// Jump
 		if (isJumpDown && rb.velocity.y == 0) {
+			// Jump from ground
 			rb.AddRelativeForce (0, jumpForceGround, 0);
 		} else if (isJumpDown) {
+			// Flap frantically in air
 			rb.AddRelativeForce (0, jumpForceAir, 0);
 		}
 	}
